be213a1712974bcd535d6898c3d036e1b8672d72
gavinchou94 <gaz11@pitt.edu>
2025-02-04 13:58:00 -0500

    Add Problem 3 postfix to prefix conversion

---
+++ Problems/problem3_postpre/postpre.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+#include "algebraic_expressions.hpp"
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cout << "Usage: " << argv[0] << " postfix" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::string postfix = argv[1];
+  std::string prefix = "";
+
+  try {
+    convert(postfix, prefix);
+    std::cout << prefix << std::endl;
+  } catch (std::invalid_argument ex) {
+    std::cerr << ex.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+};
