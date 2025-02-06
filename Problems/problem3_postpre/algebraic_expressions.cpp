7d70c4c9e2e55ad471293ae675b580fa0ba09edf
gavinchou94 <gaz11@pitt.edu>
2025-02-06 14:02:04 -0500

    add minor comments to help understand given functions

--- Problems/problem3_postpre/algebraic_expressions.cpp
+++ Problems/problem3_postpre/algebraic_expressions.cpp
@@ -12,8 +12,7 @@ bool isoperator(char ch)
 int endPost(std::string s, int last)
 {
   int first = 0;
-
-  if ((first > last))
+  if (first > last) // invalid index for last
   {
     return -1;
   }
@@ -21,16 +20,16 @@ int endPost(std::string s, int last)
   char ch = s[last];
   if (isalpha(ch))
   {
-    return last;
+    return last; // if last is an operator, then the end of the post-fix expression is just itself
   }
   else
   {
-    if (isoperator(ch))
+    if (isoperator(ch)) // can be omitted, but just to make sure that the last character is an operator
     {
-      int lastEnd = endPost(s, last - 1);
+      int lastEnd = endPost(s, last - 1); // search for the "start" (in human eyes) of first post-fix expression before ch
       if (lastEnd > -1)
       {
-        return endPost(s, lastEnd - 1);
+        return endPost(s, lastEnd - 1); // search for the "start" (in human eyes) of the second post-fix expression before ch
       }
       else
       {
@@ -47,12 +46,12 @@ int endPost(std::string s, int last)
 bool isPost(std::string s)
 {
   int firstChar = endPost(s, s.size() - 1);
-
+  // same as prefix validarity check, postfix expression is valid if and only if
+  // the post-fix expression range of the last ch happens to include until the first character
   return (firstChar == 0);
 }
 
 void convert(const std::string &postfix, std::string &prefix)
 {
-
   // TODO
 }
--- Problems/problem3_postpre/postpre.cpp
+++ Problems/problem3_postpre/postpre.cpp
@@ -1,3 +1,6 @@
+// This is the a main file to test the conversion program via command line.
+// Just for you to try, check VS Code debugger guidance on Canvas for CLI debugging.
+
 #include <cstdlib>
 #include <iostream>
 #include <string>
@@ -5,8 +8,10 @@
 
 #include "algebraic_expressions.hpp"
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
     std::cout << "Usage: " << argv[0] << " postfix" << std::endl;
     return EXIT_FAILURE;
   }
@@ -14,10 +19,13 @@ int main(int argc, char **argv) {
   std::string postfix = argv[1];
   std::string prefix = "";
 
-  try {
+  try
+  {
     convert(postfix, prefix);
     std::cout << prefix << std::endl;
-  } catch (std::invalid_argument ex) {
+  }
+  catch (std::invalid_argument ex)
+  {
     std::cerr << ex.what() << std::endl;
     return EXIT_FAILURE;
   }
